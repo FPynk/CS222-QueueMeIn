4bc515362c7456746407e28282e5e9297a46f21b
FPynk <acloh2@illinois.edu>
2023-11-11 14:25:08 -0600

    reformat

--- src/pages/CurrEventCompany/CurrentEventCompany.jsx
+++ src/pages/CurrEventCompany/CurrentEventCompany.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import NavBar from './NavBar';
+import NavBar from '../NavBar';
 
 function CurrentEventCompany() { // Purely for testing
     const [queue, setQueue] = useState([
