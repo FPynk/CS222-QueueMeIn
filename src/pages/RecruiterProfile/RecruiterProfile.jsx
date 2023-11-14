f54dc6965df9886761cad1f9687c17b3a8af9b46
dschuck2 <dschuck2@illinois.edu>
2023-11-13 20:34:28 -0600

    fixed styling

--- src/pages/RecruiterProfile/RecruiterProfile.jsx
+++ src/pages/RecruiterProfile/RecruiterProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Text, Textarea, Input, Button, VStack, Accordion,
+import { Box, Text, Textarea, Input, Button, VStack, Accordion, Heading,
          AccordionItem, AccordionButton, AccordionPanel, IconButton
 } from '@chakra-ui/react';
 
@@ -154,9 +154,9 @@ function RecruiterProfile() {
         <div className="h-screen flex flex-col items-center">
             <NavBar/>
             <Box className="app-container">
-                <Text className="app-name center" align="center">Company Profile</Text>
+                <Text className="app-name center" align="center" fontSize="3xl" >Profile</Text>
                 <VStack>
-                    <Text className="header">Company Information</Text>
+                    <Text className="header" fontSize="xl" as='u'>Company Information</Text>
                     <VStack>
                         <Input placeholder="Company Name" defaultValue={companyName}
                             onInput={(e) => setCompanyName(e.target.value)}/>
@@ -167,14 +167,14 @@ function RecruiterProfile() {
                         <Input placeholder="Company Website" type="url" defaultValue={website}
                             onInput={(e) => setWebsite(e.target.value)}/>
                     </VStack>
-                    <Text>Recruiter Information</Text>
+                    <Text fontSize="xl" as='u'>Recruiter Information</Text>
                     <VStack>
                         <Input placeholder="Recruiter Name" defaultValue={recruiterName}
                             onInput={(e) => setRecruiterName(e.target.value)}/>
                         <Input placeholder="Recruiter Phone" type="tel" defaultValue={phone}
                             onInput={(e) => setPhone(e.target.value)}/>
                     </VStack>
-                    <Text>Available Positions</Text>
+                    <Text fontSize="xl" as='u'>Available Positions</Text>
                     <Accordion allowMultiple="true">
                         {jobs.map((item, index) => 
                             <JobListing key={index} info={item} deleteJob={() => deleteJob(index)}
